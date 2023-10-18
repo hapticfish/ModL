055f918e2dd6f382080f41837204ebcef5fb5955
hapticfish <jquinmail@gmail.com>
2023-10-18 00:03:26 -0300

    updated todos

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, request, jsonify, flash
 import random, data_analysis, ml_model
 app = Flask(__name__)
+
+
+#TODO add logic in modules for processing documentation
+#TODO add logic in modules for uploading document
+#TODO add logic in modules for returning data
+#TODO add logic in modules for showing graphics do some research on how to do this
