320404f2d9056a6c368d6753b9c503b68432c1f8
hapticfish <jquinmail@gmail.com>
2023-10-22 01:37:45 -0300

    created upload to react for data

--- BackEnd/visualize/dataDisplay.py
+++ BackEnd/visualize/dataDisplay.py
@@ -1,10 +1,15 @@
 from flask import jsonify, request
 from app import app
+from BackEnd.analysis.preprocessing import upload_file
+
+"""Returns the uploaded file as json data to be displayable on the front end for review"""
 
 
 @app.route('/datasetDisplay', methods=['GET'])
 def preprocess_data():
-    return ""
+    df = upload_file()
+    json_data = df.to_json(orient='records')
+    return json_data
 
 
 @app.route('/dataVisualizations', methods=['GET'])
