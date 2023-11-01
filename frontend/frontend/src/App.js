3a08721825ac46c94a8a6cc00b4216460db1276e
hapticfish <jquinmail@gmail.com>
2023-11-01 13:08:47 -0300

    Webstorm commit FileUpload module and Header module.

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Header from "./components/Header";
+import FileUpload from "./components/FileUpload";
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+        <FileUpload />
+
+    </div>
+  );
+}
+
+export default App;
